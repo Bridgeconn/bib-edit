58773672f2ccf905cbdb1d9116c6f4ce3bddb16b
Joel Mathew <joelmathew.mec@gmail.com>
2016-07-09 02:51:43 -0700

    Connecting UI. WIP.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,98 +1,159 @@
 const session = require('electron').remote.session,
-      ipc = require('electron').ipcRenderer,
-      {Menu} = require('electron').remote,
       PouchDB = require('pouchdb');
-var bibUtil = require("../util/json_to_usfm.js");
 
-const menu = Menu.buildFromTemplate([
-    {
-        label: 'Autographa Lite'/*,
-	submenu: [
-	    {
-		label: 'Settings',
-		click: function () {
-		    ipc.sendSync('show-import-window');
-		}
+var db = new PouchDB('database'),
+    refDb = new PouchDB('reference'),
+    book,
+    chapter,
+    currentBook;
+
+document.getElementById("save-btn").addEventListener("click", function (e) {
+    var verses = currentBook.chapters[parseInt(chapter,10)-1].verses;
+    verses.forEach(function (verse, index) {
+	var vId = 'v'+(index+1);
+	verse.verse = document.getElementById(vId).textContent;
+    });
+
+    currentBook.chapters[parseInt(chapter,10)-1].verses = verses;
+    db.put(currentBook).then(function (response) {
+	console.log('Saved changes.');
+    }).catch(function (err) {
+	console.log('Error: While retrieving document. ' + err);
+    });
+});
+
+function createVerseInputs(verses, chunks, chapter) {
+    var i, chunkIndex = 0, chunkVerseStart, chunkVerseEnd;
+    for(i=0; i<chunks.length; i++) {
+	if(parseInt(chunks[i].chp, 10) === parseInt(chapter, 10)) {
+	    chunkIndex = i+1;
+	    chunkVerseStart = parseInt(chunks[i].firstvs, 10);
+	    chunkVerseEnd = parseInt(chunks[i+1].firstvs, 10) - 1;
+	    break;
+	}
+    }
+
+    for (i=1; i<=verses.length; i++) {
+	var divContainer = document.createElement('div'),
+	    spanVerseNum = document.createElement('span'),
+	    spanVerse = document.createElement('span');
+	if(i > chunkVerseEnd) {
+	    chunkVerseStart = parseInt(chunks[chunkIndex].firstvs, 10);
+	    if(chunkIndex === chunks.length-1 || parseInt((chunks[chunkIndex+1].chp), 10) != chapter) {
+		chunkVerseEnd = verses.length;
+	    } else {
+		chunkIndex++;
+		chunkVerseEnd = parseInt(chunks[chunkIndex].firstvs, 10)-1;
 	    }
-	]*/
+	}
+	var chunk = chunkVerseStart + '-' + chunkVerseEnd;
+	spanVerse.setAttribute("chunk-group", chunk);
+	spanVerse.contentEditable = true;
+	spanVerse.id = "v"+i;
+	spanVerse.appendChild(document.createTextNode(verses[i-1].verse));
+	spanVerseNum.appendChild(document.createTextNode(i));
+	divContainer.appendChild(spanVerseNum);
+	divContainer.appendChild(spanVerse);
+	document.getElementById('input-verses').appendChild(divContainer);
     }
-]);
-
-Menu.setApplicationMenu(menu);
-
-var constants = require('../util/constants.js');
-
-function createBooksList(booksLimit) {
-    var i;
-    for (i=1; i<=booksLimit; i++) {
-	b = document.createElement('button');
-	b.className = "stack pseudo button";
-	b.id = "b"+i;
-	t = document.createTextNode(constants.booksList[i-1]);
-	b.appendChild(t);
-	document.getElementById('leftpane').appendChild(b);
-    }
+    highlightRef();
 }
 
-function createChaptersList(chaptersLimit) {
-    var i;
-    for (i=1; i<=chaptersLimit; i++) {
-	c = document.createElement('button');
-	c.className = "access pseudo button";
-	t = document.createTextNode(i);
-	c.appendChild(t);
-	c.id = "c"+i;
-	document.getElementById('chapters-pane').appendChild(c);
-	c.addEventListener('click', function (e) {
-	    const cookie = {url: 'http://chapter.autographa.com', name: 'chapter', value: e.target.id.substring(1)};
-	    session.defaultSession.cookies.set(cookie, (error) => {
-		if (error)
-		    console.error(error);
+session.defaultSession.cookies.get({url: 'http://book.autographa.com'}, (error, cookie) => {
+    book = '1';
+    if(cookie.length > 0) {
+	book = cookie[0].value;
+    }
+    session.defaultSession.cookies.get({url: 'http://chapter.autographa.com'}, (error, cookie) => {
+	chapter = '1';
+	if(cookie.length > 0) {
+	    chapter = cookie[0].value;	    
+	}
+	console.log('values are ' + book + ' ' + chapter);
+	db.get(book).then(function (doc) {
+	    refDb.get('refChunks').then(function (chunkDoc) {
+		console.log(doc.chapters[parseInt(chapter,10)-1].verses.length);
+		currentBook = doc;
+		createRefSelections();
+		createVerseInputs(doc.chapters[parseInt(chapter,10)-1].verses, chunkDoc.chunks[parseInt(book,10)-1], chapter);
 	    });
-	    const reply = ipc.sendSync('synchronous-message', 'ping');
-	    const message = `Synchronous message reply: ${reply}`;
-	});
-    }
-}
-
-createBooksList(66);
-
-books = document.querySelectorAll("button[id^=b]");
-for(i=1; i<=books.length; i++) {
-    books[i-1].addEventListener("click", function (e) {
-	const cookie = {url: 'http://book.autographa.com', name: 'book', value: e.target.id.substring(1)};
-	session.defaultSession.cookies.set(cookie, (error) => {
-	    if (error)
-		console.error(error);
-	});
-	var db = new PouchDB('database');
-	db.get(e.target.id.substring(1).toString()).then(function (doc) {
-	    chaptersPane = document.getElementById("chapters-pane");
-	    while (chaptersPane.lastChild) {
-		chaptersPane.removeChild(chaptersPane.lastChild);
-	    }
-	    createChaptersList(doc.chapters.length);
-	    db.close();
 	}).catch(function (err) {
 	    console.log('Error: While retrieving document. ' + err);
-	    db.close();
-	});
-    });
-}
-
-document.getElementById("export-btn").addEventListener("click", function (e) {
-    session.defaultSession.cookies.get({url: 'http://book.autographa.com'}, (error, cookie) => {
-	console.log(cookie);
-	book = {};
-	var db = new PouchDB('database');
-	db.get('targetBible').then(function (doc) {
-	    book.bookNumber = cookie[0].value;
-	    book.bookName = constants.booksList[parseInt(book.bookNumber, 10)-1];
-	    book.bookCode = constants.bookCodesList[parseInt(book.bookNumber, 10)-1];
-	    book.outputPath = doc.targetPath;
-	    bibUtil.toUsfm(book);
-	}).catch(function (err) {
-	    console.log('Error: Cannot get details from DB');
 	});
     });
 });
+
+var bookCodeList = require('../util/constants.js').bookCodeList;
+
+function showReferenceText(ref_id) {
+    ref_id = (ref_id === 0 ? document.getElementById('refs-select').value : ref_id);
+    var id = ref_id + '_' + bookCodeList[parseInt(book,10)-1],
+	i;
+    console.log('id is = ' + id);
+    refDb.get(id).then(function (doc) {
+	for(i=0; i<doc.chapters.length; i++) {
+	    if(doc.chapters[i].chapter == parseInt(chapter, 10)) {
+		break;
+	    }
+	}
+	document.getElementById('ref').innerHTML = doc.chapters[i].verses.map(function (verse, verseNum) {
+	    return '<div id="r' + (verseNum+1) +'"><span>' + (verseNum+1) + '</span><span>' + verse.verse + '</span></div>';
+	}).join('');
+    }).catch(function (err) {
+	console.log('Error: Unable to find requested reference in DB. ' + err);
+    });
+}
+
+function createRefSelections() {
+    refDb.get('refs').then(function (doc) {
+	console.log(doc);
+	doc.ref_ids.forEach(function (ref_doc) {
+	    if(ref_doc.isDefault) {
+		$('#selected-ref').text(ref_doc.ref_name);
+		showReferenceText(ref_doc.ref_id);
+	    }
+	    var li = document.createElement('li'),
+		a = document.createElement('a');
+	    a.setAttribute('href', '#');
+	    a.setAttribute('data-value', ref_doc.ref_id);
+	    var t = document.createTextNode(ref_doc.ref_name);
+	    a.appendChild(t);
+	    li.appendChild(a);
+	    document.getElementById('refs-list').appendChild(li);
+	});
+	$('#refs-list li a').click(function() {
+	    $('#selected-ref').text($(this).text());
+	    console.log($(this).attr("data-value"));
+	    showReferenceText($(this).attr("data-value"));
+	});
+    }).catch(function (err) {
+	console.log('Info: No references found in Database. ' + err);
+    });
+}
+
+/*
+document.getElementById("refs-select").addEventListener("change", function (e) {
+    showReferenceText(e.target.value);
+});
+*/
+
+function highlightRef() {
+    var i,
+	j,
+	verses = document.querySelectorAll("span[id^=v]");
+    for(i=0; i<verses.length; i++) {
+	verses[i].addEventListener("focus", function (e) {
+	    var limits = e.target.getAttribute("chunk-group").split("-").map(function (element) {
+		return parseInt(element, 10) - 1;
+	    });;
+	    console.log(limits);
+	    var refs = document.querySelectorAll("div[id^=r]");
+	    refs.forEach(function (ref) {
+		ref.style.backgroundColor = "";
+	    });
+	    for(j=limits[0]; j<=limits[1]; j++) {
+		refs[j+1].style.backgroundColor = "#b3ffa8";
+	    }
+	});
+    }
+}
