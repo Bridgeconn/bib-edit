1fa26a9542b23c05896678daafd7dbd8a0919f67
Joel Mathew <joelmathew.mec@gmail.com>
2016-12-24 16:53:28 +0530

    Fixing seed flow.

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,18 @@
 const PouchDB = require('pouchdb');
-var db = new PouchDB('./db/targetDB');
 
 function destroyDbs() {
-    db.destroy().then(function (response) {
-	console.log(response);
-	console.log('done destroying.');
-	db.close();
+    const targetDb = new PouchDB('./db/targetDB');
+    targetDb.destroy().then(function (response) {
+	console.log('targetDB destroyed!.');
+	targetDb.close();
     }).catch(function (err) {
 	console.log(err);
-	db.close();
+	targetDb.close();
     });
 
-    var refDb = new PouchDB('./db/referenceDB');
+    const refDb = new PouchDB('./db/referenceDB');
     refDb.destroy().then(function (response) {
-	console.log(response);
-	console.log('done destroying refs.');
+	console.log('referenceDB destroyed!');
 	refDb.close();
     }).catch(function (err) {
 	console.log(err);
@@ -23,40 +21,61 @@ function destroyDbs() {
 }
 
 function setupTargetDb() {
-    db.get('isDBSetup').then(function (doc) {
-	db.close();    
-    }).catch(function (err) {
-	const bibleJson = require('../app/lib/full_bible_skel.json');
-	db.bulkDocs(bibleJson).then(function (response) {
-	    console.log(response);
-	    db.close();
-	}).catch(function (err) {
-	    console.log(err);
-	    db.close();
+    const targetDb = new PouchDB('./db/targetDB');    
+    targetDb.get('isDBSetup')
+	.then(function (doc) {
+	    targetDb.close();    
+	})
+	.catch(function (err) {
+	    const bibleJson = require('../app/lib/full_bible_skel.json');
+	    targetDb.bulkDocs(bibleJson)
+		.then(function (response) {
+		    console.log('Successfully setup Target DB.');
+		    targetDb.close();
+		})
+		.catch(function (err) {
+		    console.log(err);
+		    targetDb.close();
+		});
 	});
-    });
 }
 
 function setupRefDb() {
     var refDb = new PouchDB('./db/referenceDB');
-    refDb.get('refs').then(function (doc) {
-	refDb.close();
-    }).catch(function (err) {
-	const refEnJson = require('../app/lib/refs.json'),
-	      chunksJson = require('../app/lib/chunks.json');
-	refDb.bulkDocs(refEnJson).then(function (response) {
-		console.log(chunksJson);
-	    refDb.put(chunksJson).then(function (response) {
-		console.log(response);
-		refDb.close();	
-	    });
-	}).catch(function (err) {
-	    console.log('Error loading reference data. ' + err);
+    refDb.get('refs')
+	.then(function (doc) {
 	    refDb.close();
-	});	
-    });
+	})
+	.catch(function (err) {
+	    const refEnUlbJson = require('../app/lib/en_ulb.json'),
+		  refEnUdbJson = require('../app/lib/en_udb.json'),
+		  refHiUlbJson = require('../app/lib/hi_ulb.json'),
+		  chunksJson = require('../app/lib/chunks.json'),
+		  refsConfigJson = require('../app/lib/refs_config.json');	    
+	    refDb.put(chunksJson)	
+		.then(function (response) {
+		    return refDb.put(refsConfigJson);
+		})
+		.then(function (response) {
+		    return refDb.bulkDocs(refEnUlbJson);
+		})
+		.then(function (response) {
+		    return refDb.bulkDocs(refEnUdbJson);
+		})
+		.then(function (response) {
+		    return refDb.bulkDocs(refHiUlbJson);
+		})
+		.then(function (response) {
+		    console.log('Successfully loaded reference texts.');
+		    refDb.close();
+		})
+		.catch(function (err) {
+		    console.log('Error loading reference data.' + err);
+		    refDb.close();
+		});
+	});
 }
 
 //destroyDbs();
-setupTargetDb();
 setupRefDb();
+setupTargetDb();
