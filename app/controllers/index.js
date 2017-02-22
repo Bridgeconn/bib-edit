c8b93ccbbf9e814e09ea4f144f13e16f1cc8c57a
nbajpai9 <nbajpai9@gmal.com>
2017-02-22 19:28:41 +0530

    made some UI changes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1181,3 +1181,13 @@ function saveLastVisit(book, chapter){
     });
 }
 //save last visit end
+// $("#ex6").slider();
+// $("#ex6").on("slide", function(slideEvt) {
+//     $("#ex6SliderVal").text(slideEvt.value);
+// });
+
+// Without JQuery
+// var slider = new Slider("#ex6");
+// slider.on("slide", function(sliderValue) {
+//     document.getElementById("ex6SliderVal").textContent = sliderValue;
+// });
