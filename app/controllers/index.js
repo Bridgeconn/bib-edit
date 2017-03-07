b7f37d59ef3807b2b935446df099e4336d0d3075
nbajpai9 <nbajpai9@gmal.com>
2017-03-07 17:32:55 +0530

    pop-up settings have vertical tab

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1191,3 +1191,19 @@ function saveLastVisit(book, chapter){
 // slider.on("slide", function(sliderValue) {
 //     document.getElementById("ex6SliderVal").textContent = sliderValue;
 // });
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
